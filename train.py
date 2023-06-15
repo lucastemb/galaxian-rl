e1de39b7110887b2a9a9163edb02aa1fd3ad4e18
Lucas Tembras <lucastembras@gmail.com>
2023-06-15 13:57:37 -0400

    improved reinforcement learning code to optimize model/agent (v2)

--- train.py
+++ train.py
@@ -3,29 +3,86 @@ import os
 import numpy as np
 
 from stable_baselines3.common.monitor import Monitor
-from stable_baselines3.common.noise import NormalActionNoise
 from callback import SaveOnBestTrainingRewardCallback
-
+from stable_baselines3.common.utils import set_random_seed
+from stable_baselines3.common.vec_env import SubprocVecEnv
+from stable_baselines3.common.vec_env import VecMonitor
+from stable_baselines3.common.results_plotter import load_results, ts2xy
+from stable_baselines3.common.callbacks import BaseCallback
 from stable_baselines3 import PPO
 
-models_dir = "models/PPO"
-log_dir="logs"
 
-if not os.path.exists(models_dir):
-    os.makedirs(models_dir)
-
-if not os.path.exists(log_dir):
-    os.makedirs(log_dir)
-
-env = gym.make("ALE/Galaxian-v5")
-env=Monitor(env,log_dir)
-env.reset()
-
+MODELS_DIR = "models/PPO"
+LOG_DIR="logs"
 TIMESTEPS = 100000
 
-model = PPO("MlpPolicy", env, verbose=1, tensorboard_log=log_dir, learning_rate=0.0001)
-model.learn(total_timesteps=TIMESTEPS)
-model.save(f"{models_dir}/galaxian-ai-v5-{TIMESTEPS}")
+class SaveOnBestTrainingRewardCallback(BaseCallback):
+    """
+    Callback for saving a model (the check is done every ``check_freq`` steps)
+    based on the training reward (in practice, we recommend using ``EvalCallback``).
+
+    :param check_freq:
+    :param log_dir: Path to the folder where the model will be saved.
+      It must contains the file created by the ``Monitor`` wrapper.
+    :param verbose: Verbosity level: 0 for no output, 1 for info messages, 2 for debug messages
+    """
+    def __init__(self, check_freq: int, log_dir: str, verbose: int = 1):
+        super(SaveOnBestTrainingRewardCallback, self).__init__(verbose)
+        self.check_freq = check_freq
+        self.log_dir = log_dir
+        self.save_path = os.path.join(log_dir, "best_model")
+        self.best_mean_reward = -np.inf
+
+    def _init_callback(self) -> None:
+        # Create folder if needed
+        if self.save_path is not None:
+            os.makedirs(self.save_path, exist_ok=True)
+
+    def _on_step(self) -> bool:
+        if self.n_calls % self.check_freq == 0:
+
+          # Retrieve training reward
+          x, y = ts2xy(load_results(self.log_dir), "timesteps")
+          if len(x) > 0:
+              # Mean training reward over the last 100 episodes
+              mean_reward = np.mean(y[-100:])
+              if self.verbose >= 1:
+                print(f"Num timesteps: {self.num_timesteps}")
+                print(f"Best mean reward: {self.best_mean_reward:.2f} - Last mean reward per episode: {mean_reward:.2f}")
+
+              # New best model, you could save the agent here
+              if mean_reward > self.best_mean_reward:
+                  self.best_mean_reward = mean_reward
+                  # Example for saving best model
+                  if self.verbose >= 1:
+                    print(f"Saving new best model to {self.save_path}")
+                  self.model.save(self.save_path)
+
+        return True
+
+def make_env(env_id, rank, seed=0):
+    def _init():
+        env = gym.make(env_id)
+        env.reset(seed=seed + rank)
+        return env
+    set_random_seed(seed)
+    return _init
+
+if __name__ == "__main__":
+    if not os.path.exists(MODELS_DIR):
+        os.makedirs(MODELS_DIR)
+
+    if not os.path.exists(LOG_DIR):
+        os.makedirs(LOG_DIR)
+    
+    env_id = "ALE/Galaxian-v5"
+    num_cpu = 4
+    vec_env = VecMonitor(SubprocVecEnv([make_env(env_id, i) for i in range(num_cpu)]))
+    model = PPO("MlpPolicy", vec_env, verbose=1, tensorboard_log=LOG_DIR)
+    
+    callback = SaveOnBestTrainingRewardCallback(check_freq=1000, log_dir=LOG_DIR)
+    model.learn(total_timesteps=TIMESTEPS, callback=callback)
+    model.save(f"{MODELS_DIR}/galaxian-ai-v6-{TIMESTEPS}")
 
 
 
